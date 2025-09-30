7ef51937225538af5749e86106d09d96285a7586
kchenfs <kendxd@gmail.com>
2025-09-30 17:03:21 -0400

    explicitly import

--- src/amplify-config.tsx
+++ src/amplify-config.tsx
@@ -1,6 +1,6 @@
-// src/amplify-config.ts
+import { ResourcesConfig } from 'aws-amplify';
 
-export const amplifyConfig = {
+export const amplifyConfig: ResourcesConfig = {
   Auth: {
     Cognito: {
       userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
@@ -11,23 +11,22 @@ export const amplifyConfig = {
           scopes: ['email', 'openid', 'profile'],
           redirectSignIn: [
             'http://localhost:5173/',
-            'https://prepdeck.momotarosushi.ca/'
+            'https://prepdeck.momotarosushi.ca/',
           ],
           redirectSignOut: [
             'http://localhost:5173/',
-            'https://prepdeck.momotarosushi.ca/'
+            'https://prepdeck.momotarosushi.ca/',
           ],
           responseType: 'code',
-        }
-      }
-    }
+        },
+      },
+    },
   },
   API: {
     GraphQL: {
       endpoint: import.meta.env.VITE_APPSYNC_GRAPHQL_API_URL,
       region: import.meta.env.VITE_AWS_REGION,
-      // THIS IS THE FIX: In Amplify v6, the value is simplified to 'userPool'
-      defaultAuthMode: 'userPool'
-    }
-  }
+      defaultAuthMode: 'userPool', // now valid
+    },
+  },
 };
